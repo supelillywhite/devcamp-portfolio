30c01ccca1c3d6bff0f1e743ec19ceb289349732
Jerel Lillywhite <supelillywhite@jerels-mbp.ad.uvu.edu>
2017-12-08 08:49:47 -0700

    Added navbar and default header content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,13 @@
+module DefaultPageContent
+  extend ActiveSupport:: Concern
+
+  included do
+    before_action :set_page_defaults
+  end
+
+  def set_page_defaults
+    @page_title = "Devcamp Portfolio | Supe Lillywhite"
+    @seo_keywords = "Supe Lillywhite portfolio"
+  end
+
+end
