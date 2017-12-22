f500760e2503969b25264c08f0aae0c13c25f55c
Jerel Lillywhite <supelillywhite@gmail.com>
2017-12-22 10:59:37 -0700

    Integrated productions calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_cable.allowed_request_origins = ['https://supe-lillywhite-portfolio.herokuapp.com', 'http://supe-lillywhite-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://supe-lillywhite-portfolio.herokuapp.com/cable"
 end
